e8eacbb530742aeca8caa5947ae118a883690aec
xorblo-doitus <blabla86420@gmail.com>
2024-02-13 17:40:12 +0100

    Added a screenshot sortcut to create thumbnail

--- src/singletons/global_inputs.gd
+++ src/singletons/global_inputs.gd
@@ -13,3 +13,11 @@ func _process(_delta) -> void:
 			get_window().mode = Window.MODE_FULLSCREEN
 		else:
 			get_window().mode = Window.MODE_WINDOWED
+	
+	if Input.is_action_just_pressed("screenshot"):
+		var capture = get_viewport().get_texture().get_image()
+		var _time = Time.get_unix_time_from_system()
+		DirAccess.make_dir_recursive_absolute("user://screenshots/")
+		var filename = "user://screenshots/screenshot-{0}.png".format({"0":_time})
+		capture.save_png(filename)
+		print("screenshot saved under ", ProjectSettings.globalize_path(filename))
